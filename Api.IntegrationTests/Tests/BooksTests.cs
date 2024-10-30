1d8141c74fd90d8382bf694624ee0c2407b9b63f
Jean-Jensen <jeanjensen911@gmail.com>
2024-10-30 21:28:22 +0700

    made some more tests

--- Api.IntegrationTests/Tests/BooksTests.cs
+++ Api.IntegrationTests/Tests/BooksTests.cs
@@ -1,11 +1,13 @@
 using System.Net.Http.Json;
 using System.Text.Json;
 using Generated;
+using Xunit.Abstractions;
 
 namespace Api.IntegrationTests.Tests;
 
-public class BooksTests : ApiTestBase
+public class BooksTests(ITestOutputHelper output ) : ApiTestBase
 {
+    
     [Fact]
     public async Task CreateBook_CanSuccessFullyCreateBook()
     {
@@ -21,4 +23,68 @@ public class BooksTests : ApiTestBase
         Assert.Equivalent(result.Title, dto.Title);
         Assert.NotEqual(0, result.Id);
     }
+
+    [Fact]
+    public async Task LoanDoesntHappen_IfAlreadyLoaned()
+    {
+
+        var dto = new CreateBookDto()
+        {
+            Author = "B",
+            Genre = "B",
+            Title = "B"
+        };
+        
+        var BookResult = (await new LibraryClient(Client).PostAsync(dto)).Result;
+        output.WriteLine(BookResult.Loans.ToString());
+
+        var LoanDto = new LoanBookDto()
+        {
+            BookId = BookResult.Id,
+            UserId = 1
+        };
+        
+        var result = (await new LibraryClient(Client).LoanAsync(LoanDto)).Result;
+        
+        
+        var LoanDto2 = new LoanBookDto()
+        {
+            BookId = BookResult.Id,
+            UserId = 1
+        };
+        
+        var result2 = (await new LibraryClient(Client).LoanAsync(LoanDto2)).Result;
+        
+        
+        Assert.NotNull(result);
+        //Assert.NotNull(result2);
+
+    }
+    
+
+    
+    [Fact]
+    public async Task BookDoesntCreate_WithEmptyFields()
+    {
+        var dto = new CreateBookDto()
+        {
+            Author = "",
+            Genre = "",
+            Title = "failing book"
+        };
+
+        try
+        {
+            var result = (await new LibraryClient(Client).PostAsync(dto));
+            Assert.False(result.StatusCode == 200);
+        }
+        catch (ApiException ex)
+        {
+            Assert.Equal(400, ex.StatusCode);
+        }
+        
+
+    }
+    
+    
 }
